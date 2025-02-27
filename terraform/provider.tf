c121e5b93857cd40de8dba95240d36d3d4bd7a7d
Patrick <patrick_jose_00@hotmail.com>
2025-02-27 22:23:12 +0100

    Terraform Azure Provider

---
+++ terraform/provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  client_id       = var.az_client_id
+  client_secret   = var.az_client_secret
+  subscription_id = var.az_subscription_id
+  tenant_id       = var.az_tenant_id
+}
