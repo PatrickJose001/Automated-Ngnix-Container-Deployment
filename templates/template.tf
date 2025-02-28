529ad8b1ae3e09d78466c64a49a0053d16fc64f6
Patrick <patrick_jose_00@hotmail.com>
2025-02-28 16:17:12 +0100

    Template for ACI

---
+++ templates/template.tf
@@ -0,0 +1,35 @@
+
+
+resource "azurerm_container_group" "${unique_name}" {
+  name                = "${unique_name}-${random_id}"
+  location            = azurerm_resource_group.nginx_rg.location
+  resource_group_name = azurerm_resource_group.nginx_rg.name
+  os_type             = "Linux"
+  ip_address_type     = "Public"
+  network_profile_id  = azurerm_network_profile.nginx_profile.id
+
+  container {
+    name   = "${unique_name}"
+    image  = "nginx:latest"
+    cpu    = 0.5
+    memory = 1.5
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "development"
+  }
+
+  image_registry_credential {
+    server   = "index.docker.io"
+    username = var.docker_username
+    password = var.docker_password
+}
+
+
+  depends_on = [azurerm_subnet.aci_subnet]  # Ensures subnet is created before the container group
+}
