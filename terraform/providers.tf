47aeb858faee418e10995cb45ef1a715b2d235fd
Patrick <patrick_jose_00@hotmail.com>
2025-02-28 17:39:10 +0100

    testing removing varaibles

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,24 @@
+# provider "azurerm" {
+#   features {}
+
+#   client_id       = var.AZURE_CLIENT_ID
+#   client_secret   = var.AZURE_CLIENT_SECRET
+#   tenant_id       = var.AZURE_TENANT_ID
+#   subscription_id = var.AZURE_SUBSCRIPTION_ID
+# }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-
-  # client_id       = var.AZURE_CLIENT_ID
-  # client_secret   = var.AZURE_CLIENT_SECRET
-  # tenant_id       = var.AZURE_TENANT_ID
-  # subscription_id = var.AZURE_SUBSCRIPTION_ID
 }
