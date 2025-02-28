f3930dca4b83da132935caf2d63b87586aaef92c
Patrick <patrick_jose_00@hotmail.com>
2025-02-28 17:35:15 +0100

    testing removing varaibles

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,6 @@
 variable "DOCKER_USERNAME" {}
 variable "DOCKER_PASSWORD" {}
-variable "AZURE_CLIENT_ID" {}
-variable "AZURE_CLIENT_SECRET" {}
-variable "AZURE_TENANT_ID" {}
-variable "AZURE_SUBSCRIPTION_ID" {}
+# variable "AZURE_CLIENT_ID" {}
+# variable "AZURE_CLIENT_SECRET" {}
+# variable "AZURE_TENANT_ID" {}
+# variable "AZURE_SUBSCRIPTION_ID" {}
